46f5c6ac764b7274a9164e7233dfc482707943bf
liguilin <15554153@qq.com>
2020-07-03 16:47:22 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Link, NavLink, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Index/index' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home_C1 = () => (
+  <div>
+    <h2>Home_C1</h2>
+  </div>
+)
+const Home_C2 = () => (
+  <div>
+    <h2>Home_C2</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Product = () => (
+  <div>
+    <h2>Product</h2>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavLink className="linkRouter" activeClassName="activeLinkRouter" to="/home">Home</NavLink>
+          <NavLink className="linkRouter" activeClassName="activeLinkRouter" to="/About">About</NavLink>
+          <NavLink className="linkRouter" activeClassName="activeLinkRouter" to="/Product">Product</NavLink>
+          <hr/>
+          <Route path="/home" exact component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/product" component={Product}></Route>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Index/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { homeData:'homeData' }
+    }
+    render() { 
+        return ( <div>Home</div> );
+    }
+}
+ 
+export default Home;
